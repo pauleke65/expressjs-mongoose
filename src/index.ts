66831e33a191aa825f70c8ec1c92652e76db341b
Paul Imoke <pauleke65@gmail.com>
2025-01-28 13:16:44 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import './lib/db';
+import express from 'express';
+import countryRoutes from './routes/country';
+import { main } from './main';
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
+
+main();
+
+app.get('/', async (req, res) => {
+  res.json({ message: 'Please visit /countries to view all the countries' });
+});
+
+app.use('/countries', countryRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
