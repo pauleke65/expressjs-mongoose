e9eadf47695ce137a575e1b57ad6363d8ef5e792
Paul Imoke <pauleke65@gmail.com>
2025-01-28 15:00:51 +0100

    test gen

--- src/gen-data/constants.ts
+++ src/gen-data/constants.ts
@@ -6,12 +6,6 @@ export const SOLANA_IMAGE_PLACEHOLDER =
 
 export const MAX_TRANSACTIONS = 1500;
 
-// export const HELIUS_API_KEY = process.env.HELIUS_API_KEY || '';
-// export const SOLCAN_API_TOKEN = process.env.SOLCAN_API_TOKEN || '';
-// export const MONGO_URL = process.env.MONGO_URL || '';
-
-export const HELIUS_API_KEY = 'e8c11335-d648-46be-a5b8-2efb5c9bc35e';
-export const SOLCAN_API_TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE3MzM1NjA0NTg2OTAsImVtYWlsIjoiYW5kcmV3QGRhY2NyZWQuY28iLCJhY3Rpb24iOiJ0b2tlbi1hcGkiLCJhcGlWZXJzaW9uIjoidjIiLCJpYXQiOjE3MzM1NjA0NTh9.u5GTDuotH2q8SmY6Zp3fXi5nu8bwJIEFsD0lp2KCpEI';
-export const MONGO_URL =
-  'mongodb://mongo:MonGeSKXTkmOArtxCOQZDAjvdigkIxLy@junction.proxy.rlwy.net:30263';
+export const HELIUS_API_KEY = process.env.HELIUS_API_KEY || '';
+export const SOLCAN_API_TOKEN = process.env.SOLCAN_API_TOKEN || '';
+export const MONGO_URL = process.env.MONGO_URL || '';
